5c79f55689f1dc593e1d86a6cc66885314fa8bb5
SaBobic <fabrizioettori@gmail.com>
2022-06-30 16:39:22 +0200

    fixed milestone 2&3 using dom api

--- js/script.js
+++ js/script.js
@@ -15,22 +15,24 @@ Al click del bottone play, vengono generate 100 celle in 10 righe da 10 celle ci
 // Dichiaro le variabili globali
 const playButton = document.getElementById('play-button');
 const boxElement = document.getElementById('box');
-let grid;
+let cell;
 const boxRows = 10;
 const boxCols = 10;
 
-// Creo una funzione che mi permetta di creare un numero ben definito di celle secondo il quantitativo di righe e colonne
-const createGrid = (rows, cols) => {
-    let grid = '';
+// Creo una funzione che mi permetta di creare un div con classe cell
+const createCell = content => {
+    const cell = document.createElement('div');
+    cell.className = 'cell';
+    cell.dataset.number = content;
     //* MILESTONE 3
-    for (let i = 1; i <= rows * cols; i++){
-        grid += `<div class="cell">${i}</div>`;
-    }
-    return grid;
+    cell.innerText = content;
+    return cell;
 }
 
-// Uso il metodo addEeventListener per generate le celle al click del bottone play
+// Uso il metodo addEeventListener per generate tot celle al click del bottone play
 playButton.addEventListener('click', function(){
-    grid = createGrid (boxRows, boxCols);
-    boxElement.innerHTML = grid;
+    for (let i = 1; i <= boxRows * boxCols; i++){
+        cell = createCell(i);
+        boxElement.appendChild(cell);
+    }
 });
